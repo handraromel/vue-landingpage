1d3c8545ff70a5988176205199a48f8abad6bbb9
ArdianR <fianr5750@gmail.com>
2019-08-16 08:58:03 +0700

    First commit of customized landing page

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-toolbar color="white">
+        <v-toolbar-title v-text="title"></v-toolbar-title>
+    </v-toolbar>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                title: "Landing Page"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
